496cca54071417a9870aedbcc1fc0189fc215dc1
EC2 Default User <ec2-user@ip-172-31-17-126.ap-northeast-1.compute.internal>
2022-06-24 03:34:41 +0000

    change title

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,7 +38,7 @@ export default function Layout() {
                 <HomeIcon />
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                TEST
+                YANG EKS Hands on
               </Typography>
             </Toolbar>
           </AppBar>
